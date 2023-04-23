874041dce7e6d4363fb60ed06eb07f452aa7955d
murchie85 <murchie85@gmail.com>
2023-04-23 14:54:47 +0100

    more

---
+++ src/main/java/com/example/logManagerApp/LogEntry.java
@@ -0,0 +1,30 @@
+package com.example.logManagerApp;
+import java.util.UUID;
+import java.time.LocalDateTime;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+public class LogEntry {
+    public UUID uuid = UUID.randomUUID();
+    public LocalDateTime timestampLocal = LocalDateTime.now();
+    public enum LogLevel{
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL
+    };
+    public LogLevel logLevel;
+    public String message;
+
+
+    public LogEntry(UUID uuid, LocalDateTime timestampLocal, LogLevel logLevel, String message){
+        this.uuid = uuid;
+        this.timestampLocal = timestampLocal;
+        this.logLevel = logLevel;
+        this.message = message;
+    }
+
+}
